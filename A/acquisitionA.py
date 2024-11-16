d49656d943d5388356ec2460c7d56a7f4994ee30
Ronaldo Baker <r.remario.baker@gmail.com>
2024-11-16 19:43:09 +0000

    feat: change return type when loading BreastMNIST data

--- A/acquisitionA.py
+++ A/acquisitionA.py
@@ -1,17 +1,17 @@
 # Import dependencies
 import numpy as np
 from numpy.typing import ArrayLike
-from typing import List
+from typing import Dict
 
-def load_breastmnist_data(datapath: str) -> List[ArrayLike]:
+def load_breastmnist_data(datapath: str) -> Dict[str, ArrayLike]:
     """
     Loads the compressed BreastMNIST dataset from MedMNIST which is saved locally and returns a list of numpy arrays from the dataset. 
 
-    Args:
+    Arg:
     - datapath (str): relative or absolute path to stored dataset
 
     Returns:
-    - List[ArrayLike]: A list of all numpy arrays extracted from the compressed numpy dataset
+    - Dict[str, ArrayLike]: A dictionary of (string, numpy array) pairs
     """
     with np.load(datapath) as breastmnist_data:
         # Image data
@@ -24,5 +24,10 @@ def load_breastmnist_data(datapath: str) -> List[ArrayLike]:
         val_labels = breastmnist_data["val_labels"]
         test_labels = breastmnist_data["test_labels"]
 
-    return [train_data, train_labels, val_data, val_labels, test_data, test_labels]
+    return {"train_data": train_data,
+            "train_labels": train_labels,
+            "val_data": val_data,
+            "val_labels": val_labels, 
+            "test_data": test_data,
+            "test_labels": test_labels}
 
