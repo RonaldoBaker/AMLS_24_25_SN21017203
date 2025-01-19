e23d07d6b646321e812ac576358470456cb1ad50
Ronaldo Baker <r.remario.baker@gmail.com>
2025-01-19 11:36:44 +0000

    feat: add preprocessing function for cnn task B

--- B/preprocessingB.py
+++ B/preprocessingB.py
@@ -1,4 +1,5 @@
 from numpy.typing import ArrayLike
+from typing import List
 
 def preprocess_for_traditional(data: tuple[ArrayLike, ArrayLike]) -> ArrayLike:
     """
@@ -16,6 +17,22 @@ def preprocess_for_traditional(data: tuple[ArrayLike, ArrayLike]) -> ArrayLike:
         # Reshape each array from 3D to 2D numpy arrays for traditional machine learning models
         data[i] = data[i].reshape(data[i].shape[0], -1)
 
+        # Normalize pixel values to the range [0, 1]
+        data[i] = data[i] / 255.0
+
+    return data
+
+def preprocess_for_cnn(data: List[ArrayLike]) -> List[ArrayLike]:
+    """
+    Prepares data for CNN models by normalising pixel values.
+
+    Arg(s):
+    - data (List[ArrayLike]): The data to be preprocessed
+
+    Returns:
+    - tuple[List[ArrayLike], List[ArrayLike], List[ArrayLike]: The preprocessed train, test, and validation data
+    """
+    for i in range(len(data)):
         # Normalize pixel values to the range [0, 1]
         data[i] = data[i] / 255.0
 
